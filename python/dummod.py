9de8697b228fd9a160ab2133401cce8ff153b608
Ricardo Hernandez <isc.hernandez@gmail.com>
2019-09-03 00:37:38 -0500

    updating python codes

---
+++ python/dummod.py
@@ -0,0 +1,10 @@
+import sys
+
+myvar = 'hell!'
+
+a = 'La'
+b = 'Historia'
+c = 'de'
+d = 'Napoleon'
+
+print(a,b,c,d)
--- python/main.py
+++ python/main.py
@@ -1,1 +1,35 @@
-print ("Foo!")
+#!/usr/bin/python
+import math 
+import random
+
+#To import a specific element from a module
+#from dummod import myvar
+#print(myvar)
+#print (dummod.a,dummod.b,dummod.c,dummod.d)
+#Print options from a module
+#print(dir(dummod))
+
+#Use module directly without import
+#exec (open('dummod.py').read())
+print(math.pi)
+
+print(random.random())
+print(random.choice([1,2,3,4]))
+
+S='MyString'
+vl = list(S)
+print(vl)
+
+for i in vl:
+    print(i)
+
+print(S.find('My'))
+print(S.replace('String','Cadena'))
+print(S.upper())
+print(S.isalnum())
+
+
+vlPipeVar = 'a|b|c|d|e'
+
+
+
